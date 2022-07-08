3b32d334bffa8e7dae52c3cb77b67eeea8123b07
naekang <rlawlsgh6306@gmail.com>
2022-07-08 12:14:08 +0900

    feat: 앨범 및 수록곡 등록 태스트
    - 앨범 및 수록곡 등록 테스트

--- src/main/java/com/song/songv1/api/domain/song/Song.java
+++ src/main/java/com/song/songv1/api/domain/song/Song.java
@@ -1,10 +1,14 @@
 package com.song.songv1.api.domain.song;
 
 import com.song.songv1.api.domain.album.Album;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Song {
 
     @Id
@@ -27,6 +31,22 @@ public class Song {
 
     private String songUrl;
 
+    @JoinColumn(name = "album_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private Album album;
+
+    @Builder
+    public Song(Boolean isTitle, String name, String lyrics, String lyricWriter, String songWriter, String arranger, String songUrl, Album album) {
+        this.isTitle = isTitle;
+        this.name = name;
+        this.lyrics = lyrics;
+        this.lyricWriter = lyricWriter;
+        this.songWriter = songWriter;
+        this.arranger = arranger;
+        this.songUrl = songUrl;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
 }
